06081329112ab6f4ae284467273e3dd2e09caa01
Rafael Leite <r.cmgleite@gmail.com>
2015-10-24 13:32:10 -0200

    refactor - config updated

--- config.go
+++ config.go
@@ -1,24 +1,8 @@
 package main
 
-import (
-	"fmt"
-
-	"gopkg.in/gcfg.v1"
-)
-
 type Config struct {
 	Server struct {
 		Addr string
 		Port string
 	}
-}
-
-func LoadConfig() (*Config, error) {
-	cfg := Config{}
-	err := gcfg.ReadFileInto(&cfg, "notification_config.gcfg")
-	if err != nil {
-		fmt.Println("Error while loading config \n", err)
-		return nil, err
-	}
-	return &cfg, nil
 }
--- server.go
+++ server.go
@@ -1,39 +1,65 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/asvins/common_io"
 	"github.com/asvins/notification/mailer"
+	"github.com/asvins/utils/config"
 	"github.com/rcmgleite/router"
 )
 
 func main() {
-	r := router.NewRouter()
-	r.AddRoute("/api/discovery", router.GET, func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "NOTIFICATION MODULE - TODO")
-	})
-	http.Handle("/", r)
 
 	topics := make(map[string]common_io.CallbackFunc)
 	topics["send_mail"] = mailer.SendMail
 
-	cfg, err := common_io.LoadConfig()
+	cfg := common_io.Config{}
+	err := config.Load("common_io_config.gcfg", &cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
 	cfg.ModuleName = "notification"
 	cfg.Topics = topics
 
-	common_io.Setup(cfg)
+	common_io.Setup(&cfg)
 	defer common_io.TearDown()
 
-	serverConf, err := LoadConfig()
+	serverConf := Config{}
+	err = config.Load("notification_config.gcfg", &serverConf)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	r := router.NewRouter()
+	r.AddRoute("/api/discovery", router.GET, func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "NOTIFICATION MODULE - TODO")
+	})
+
+	// TODO remover
+	r.AddRoute("/api/mailTest", router.GET, func(w http.ResponseWriter, r *http.Request) {
+		m := mailer.Mail{
+			To:      []string{"asvins.poli@gmail.com"},
+			Subject: "Test from Asvins server",
+			Body:    "Test Message from Asvins Servers.\n -- Asvins Team",
+		}
+
+		b, err := json.Marshal(&m)
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
+			return
+		}
+
+		common_io.Publish("send_mail", b)
+		fmt.Fprintf(w, ">>Send_mail message 1 published!")
+
+	})
+
+	http.Handle("/", r)
+
 	fmt.Println("Server running on port: ", serverConf.Server.Port)
 
 	http.ListenAndServe(":"+serverConf.Server.Port, nil)
